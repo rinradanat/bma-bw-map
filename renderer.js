d3fdfbbcf4809c24ed09773f9c9c0f7b13213829
rinradanat <52401771+rinradanat@users.noreply.github.com>
2019-09-06 12:49:38 +0700

    Add files via upload
    updated

--- renderer.js
+++ renderer.js
@@ -1,14 +1,15 @@
 const electron = require('electron');
 const ipc = require('electron').ipcRenderer;
 const path = require('path');
-const BrowserWindow = electron.remote.BrowserWindow
+const BrowserWindow = electron.remote.BrowserWindow;
 
 const fs = require('fs');
 const xlsx = require('xlsx');
+const exceljs = require('exceljs');
 
-const openBtn = document.getElementById('upload')
+    const openBtn = document.getElementById('upload')
 
-openBtn.addEventListener('click', function(event){
+    openBtn.addEventListener('click', function(event){
 
     ipc.send('open-file-dialog-for-file')
     console.log('opening...');
@@ -18,21 +19,62 @@ openBtn.addEventListener('click', function(event){
 
     fs.readFile(path, 'utf-8', (err, data) => {
       if(err){
-            alert("An error ocurred reading the file :" + err.message);
-            return;
+          alert("An error ocurred reading the file :" + err.message);
+          return;
         }
 
-    //  console.log(data);
-
-    console.log('xlsx runs here');
-
-    // read xlsx file from path
+    // *read xlsx file from path
 
     const workbook = xlsx.readFile(path);
-    const sheet_name_list = workbook.SheetNames;
+    const sheetNameList = workbook.Sheets[workbook.SheetNames[0]];
+    // const ws = workbook.Sheets["Sheet2"];
 
-    console.log(xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]));
+    const fileData = xlsx.utils.sheet_to_json(sheetNameList); // in json - no row/col
+    const headerList = xlsx.utils.sheet_to_json(sheetNameList, {header: 1}); // use raw:false for formatted string ex. %
+    [ ['A', 'B'],
+      ['1', '2']]
 
+    // *show by header names
+    // console.log(headerList);
+
+    // *show contents in cells
+    console.log(fileData);
+
+      // *show outputs of Site on html
+
+      // var nextSiteUtl = fileData[i+1].Utilization ;
+
+      // var output = '';
+      var newUtlOne = [];
+      var newUtlTwo = [];
+
+      for ( var i = 0;i < fileData.length;i++){
+
+        var siteName = fileData[i].Site;
+        var siteUtl = fileData[i].Utilization * 100;
+
+        if(siteName.includes(1)){
+          var siteOneUtl = siteUtl;
+          newUtlOne.push(siteOneUtl)
+          // console.log(siteName +' utlization is '+ siteOneUtl + '%');
+          // document.getElementById('filedataShow').innerHTML = siteName + siteOneUtl;
+        } else if (siteName.includes(2)){
+          var siteTwoUtl = siteUtl;
+          newUtlTwo.push(siteTwoUtl)
+          // console.log(siteName +' utlization is '+ siteTwoUtl + '%');
+          // document.getElementById('filedataShow').innerHTML = siteName + siteTwoUtl;
+          }
+            // var output = output + '<li>'+ fileData[i].Utilization +'</li>';
+            // document.getElementById('filedataShow').innerHTML = output;
+          }
+
+          console.log(newUtlOne);
+          console.log(newUtlTwo);
       })
-    });
+    })
   })
+
+        // var myJSON = JSON.stringify(fileData);
+        // console.log(myJSON); // *show data as string
+
+        // *function for finding values in each column
