3af74afe65ee90bf15b5daa2294ddcc08587b995
rinradanat <52401771+rinradanat@users.noreply.github.com>
2019-09-13 15:09:29 +0700

    Add files via upload
    new main for ipc

--- main.js
+++ main.js
@@ -10,10 +10,11 @@ process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 // const formidable = require('formidable');
 // const fs = require('fs');
 
-const {app, BrowserWindow, Menu, dialog } = require('electron');
+const {app, BrowserWindow, Menu, dialog,webContents } = require('electron');
 const os = require('os');
 
   let mainWindow;
+  let importWindow;
 
     // Listen for the app to be ready
     app.on('ready', function(){
@@ -22,9 +23,7 @@ const os = require('os');
       // width: 600,
       // height: 425,
       resizable: true,
-      webPreferences: {
-        nodeIntegration: true,
-  }
+      webPreferences: { nodeIntegration: true}
       });
       //load html into window
       mainWindow.loadURL(url.format({
@@ -37,8 +36,9 @@ const os = require('os');
       const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
       // insert MenuMenu
       Menu.setApplicationMenu(mainMenu);
-
     });
+
+
 
 // create menu template
 
@@ -64,7 +64,7 @@ const os = require('os');
     }
   ];
 
-// open dialog
+      // open dialog
 
       ipc.on('open-file-dialog-for-file', function (event) {
         if(os.platform() === 'linux' || os.platform() === 'win32'){
@@ -80,7 +80,32 @@ const os = require('os');
                if (files) event.sender.send('selected-file', files[0]);
            });
        }});
-    
+
+       // show new win
+       ipc.on('show-import-window', function(event, newData){
+         // load imported window
+         importWindow = new BrowserWindow({
+           // parent: mainWindow,
+           // modal:true,
+           webPreferences: { nodeIntegration: true}})
+         importWindow.loadURL(url.format({
+           pathname: path.join(__dirname, 'src/imported.html'),
+           protocol: 'file:',
+           slashes: true,
+           // show: false
+         }));
+
+           console.log(newData);
+
+         ipc.on('req-new-data', function(event) {
+           importWindow.webContents.send('send-to-import', newData)
+           console.log('sent!');
+         // });
+       })
+          importWindow.on('closed', function () {
+          importWindow = null;
+      })
+     });
 
 // add dev tools
 
@@ -100,6 +125,4 @@ const os = require('os');
         }
       ]
     });
-  }
-
-console.log('working on main.js');
+  };
