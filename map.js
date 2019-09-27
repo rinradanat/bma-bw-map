a371cf4c340e968be8c3c72bb1868b08698ddf55
rinradanat <52401771+rinradanat@users.noreply.github.com>
2019-09-27 17:07:26 +0700

    Add files via upload

--- map.js
+++ map.js
@@ -49,16 +49,41 @@ const {webContents} = require('electron');
        for (var i = 0; i < sites.length; i++) {
 
             if (newData[i][1] >= 80) {
-              div.innerHTML += '<img id="'+sites[i]+'" src="'+redSrc[i]+'"/>';
+              div.innerHTML += '<img id="'+sites[i]+'" src="'+redSrc[i]+'" data-tooltip="'+sites[i] +' '+newData[i][1] + '%" />';
             } else if (newData[i][1] < 50) {
-              div.innerHTML += '<img id="'+sites[i]+'" src="'+greenSrc[i]+'"/>';
+              div.innerHTML += '<img id="'+sites[i]+'" src="'+greenSrc[i]+'"/>'; //data-tooltip="'+sites[i] +' '+newData[i][1] + '%" />';
             } else if (newData[i][1] >= 50 && newData[i][1] < 80) {
-              div.innerHTML += '<img id="'+sites[i]+'" src="'+yellowSrc[i]+'"/>';
+              div.innerHTML += '<img id="'+sites[i]+'" src="'+yellowSrc[i]+'"/>'; //data-tooltip="'+sites[i] +' '+newData[i][1] + '%" />';
             }
-            document.getElementById(sites[i]).className = sites[i] + ' site simptip-position-top';
-            document.getElementById(sites[i]).title = sites[i] +' '+newData[i][1] + '%'
+            document.getElementById(sites[i]).className = sites[i] + ' hvr-grow hover-reveal';
+            // document.getElementById(sites[i]).onmouseover = function(){
+	          //      this.setAttribute('data-tooltip', 'test')}
+            // document.getElementById(sites[i]).title = sites[i] +' '+newData[i][1] + '%';
+            // document.getElementById(sites[i]).setAttribute('data-tooltip', sites[i] +' '+newData[i][1] + '%');
        }
 
+      let setUpToolTip = function() {
+        let tooltip = "",
+            toolTipDiv = document.querySelector(".div-tooltip"),
+            toolTipElements = Array.from(document.querySelectorAll(".hover-reveal"));
 
+        let displayToolTip = function(e, obj) {
+          tooltip = obj.dataset.tooltip;
+          toolTipDiv.innerHTML = tooltip;
+          toolTipDiv.style.top = e.pageY + "px";
+          toolTipDiv.style.left = e.pageX + "px";
+          toolTipDiv.style.opacity = 1;
+          toolTipDiv.style.property = "inherit";
+        }
 
+        toolTipElements.forEach(function(elem) {
+          elem.addEventListener("mouseenter", function(e) {
+              displayToolTip(e, this);
+          });
+          elem.addEventListener("mouseleave", function(e) {
+              toolTipDiv.style.opacity = 0;
+          });
+        })
+      }
+      setUpToolTip();
     });
